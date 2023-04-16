1b3b507dfee1aa179834ea9f30ff364fd8e37ff5
Liam McCarthy <ltmccarthy9@gmail.com>
2023-04-15 20:49:53 -0500

    header and search form

--- gif-library/src/App.tsx
+++ gif-library/src/App.tsx
@@ -1,14 +1,25 @@
+
 import { useState } from 'react'
 
-
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <main className='h-screen w-100% '>
-        <header className='w-full text-center'>
+    <main className='h-screen w-100% m-4'>
+        <header className='w-full text-center mt-8'>
           <h1 className='font-bold'>Gif Library</h1>
         </header>
+
+        <form className='flex justify-center items-center m-6 '>
+          <input className='p-2 rounded-sm' placeholder='search gifs...'/>
+          <button type='submit' className='p-2 bg-red-300 font-bold text-gray-700 rounded-sm
+          hover:bg-red-400 active:bg-red-500 '>Go</button>
+        </form>
+
+        <section>
+
+        </section>
+
     </main>
   )
 }
