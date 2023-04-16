c7b8736725cfb4aa3a381edfec5cfe1130bb5b07
Liam McCarthy <ltmccarthy9@gmail.com>
2023-04-15 22:40:40 -0500

    nav and mobile

--- gif-library/src/App.tsx
+++ gif-library/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
   async function getGifs(e: FormEvent<HTMLFormElement>){
     e.preventDefault()
     try {
-      const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=25&offset=0&rating=g&lang=en`,{
+      const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=50&offset=0&rating=g&lang=en`,{
         headers: {
           'Content-Type': 'application/json'
         }
@@ -25,21 +25,19 @@ function App() {
     }
   }
 
-  // function copyGif(){
-  //   navigator.clipboard.writeText();
-  // }
 
   return (
-    <main className='h-screen w-100% my-4 xl:mx-60'>
-        <header className='w-full text-center mt-8'>
-          <h1 className='font-bold'>Gif Library</h1>
-        </header>
+    <main className='h-screen w-100% xl:mx-60 pt-20'>
+        <nav className='w-full text-center flex pt-2 pb-4 px-2 fixed top-0 z-10 bg-[#242424]'>
+          <h1 className='font-bold text-3xl mt-2'>Gif Library</h1>
+          <form onSubmit={(e) => getGifs(e)} className='flex justify-center items-center mx-4 mt-2'>
+            <input value={query} onChange={(e) => setQuery(e.target.value)} className='p-2 rounded-sm' placeholder='search gifs...'/>
+            <button type='submit' className='py-2 px-4 bg-red-300 font-bold text-gray-700 rounded-sm
+             hover:bg-red-400 active:bg-red-500 ease-in duration-75 '>Go</button>
+          </form>
+        </nav>
 
-        <form onSubmit={(e) => getGifs(e)} className='flex justify-center items-center m-6 '>
-          <input value={query} onChange={(e) => setQuery(e.target.value)} className='p-2 rounded-sm' placeholder='search gifs...'/>
-          <button type='submit' className='py-2 px-4 bg-red-300 font-bold text-gray-700 rounded-sm
-          hover:bg-red-400 active:bg-red-500 ease-in duration-75 '>Go</button>
-        </form>
+      
 
         <section className='grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5'>
           {gifs.length > 0 ? gifs.map((gif) => (
